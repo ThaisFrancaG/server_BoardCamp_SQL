d3f4dba67cffb6d74bbc994e94e030bb448f14fa
Thais <tfrancag@gmail.com>
2022-03-03 14:45:54 -0300

    Feat. Adicionadas validaçòes de formato para post/Games dentro do middleware

--- src/middleware/gamesValidation.js
+++ src/middleware/gamesValidation.js
@@ -3,6 +3,15 @@ export function validateGameSchema(schema) {
     const validation = schema.validate(req.body);
     console.log(req.body);
 
+    const stockTotal = parseInt(req.body.stockTotal);
+    const pricePerDay = parseInt(req.body.pricePerDay);
+
+    if (stockTotal !== stockTotal || pricePerDay !== pricePerDay) {
+      return res.sendStatus(422);
+    }
+    if (stockTotal <= 0 || pricePerDay <= 0) {
+      return res.status(403).send("Estoque e preço devem ser maiores do que 0");
+    }
     if (validation.error) {
       res.sendStatus(400);
       return;
