324e033cf1854619bef59c210e6ea2cd2781745e
Thais <tfrancag@gmail.com>
2022-03-05 14:41:29 -0300

    feat. Adicionados padrões de validação em CustomerSchema

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -2,9 +2,18 @@ import joi from "joi";
 
 const customerSchema = joi.object({
   name: joi.string().required(),
-  phone: joi.string().required(),
-  cpf: joi.string().required(),
-  birthday: joi.string().required(),
+  phone: joi
+    .string()
+    .pattern(/^[0-9]{10,11}$/)
+    .required(),
+  cpf: joi
+    .string()
+    .pattern(/^[0-9]{11}$/)
+    .required(),
+  birthday: joi
+    .string()
+    .pattern(/^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/)
+    .required(),
 });
 
 export default customerSchema;
