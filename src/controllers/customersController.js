c3e4ef0542fbd6330147b25a0696541fefc8753e
Thais <tfrancag@gmail.com>
2022-03-05 14:54:42 -0300

    feat. Adicionadas funções de Insert e conferência de CPF na database

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -2,7 +2,22 @@ import connection from "../database.js";
 
 export async function postCustomer(req, res) {
   try {
-    res.status(201).send("Foi criado");
+    const { name, phone, cpf, birthday } = req.body;
+
+    const checkCPFRepetition = await connection.query(
+      `SELECT * FROM customers WHERE cpf = $1`,
+      [cpf]
+    );
+
+    if (checkCPFRepetition.rows.length > 0) {
+      return res.status(409).send("CPF já cadastrado");
+    }
+
+    await connection.query(
+      `INSERT INTO customers (name,phone,cpf,birthday) VALUES ($1,$2,$3,$4)`,
+      [name, phone, cpf, birthday]
+    );
+    res.sendStatus(201);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
--- src/middleware/customerValidation.js
+++ src/middleware/customerValidation.js
@@ -2,14 +2,9 @@ export function validateCustomerSchema(schema) {
   return (req, res, next) => {
     console.log(req.body);
     const { name, phone, cpf, birthday } = req.body;
-    console.log(typeof name);
-    console.log(typeof phone);
-    console.log(typeof cpf);
-    console.log(typeof birthday);
 
     const validation = schema.validate(req.body);
     if (validation.error) {
-      console.log("deu erro genérico");
       return res.sendStatus(400);
     }
     next();
