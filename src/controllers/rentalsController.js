73060754145fe83d21d1b1ec02d0ce2d0920e83a
Thais <tfrancag@gmail.com>
2022-03-05 22:19:51 -0300

    feature. Adicionada função de inserção de novo aluguel em rentals e atualização de estoque de jogos em postRentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,18 +1,14 @@
 import connection from "../database.js";
+import dayjs from "dayjs";
 
 export async function postRental(req, res) {
   try {
     const { customerId, gameId, daysRented } = req.body;
-    //tem que adicionar coisas no body antes de enviar pra db. Suponho que é aqui que entram os joins
-    //COMEÇAR A CRIAR O OBJETO FINAL
-
-    const rentDate = Date();
 
     const checkCustomer = await connection.query(
       `SELECT * FROM customers WHERE id = $1`,
       [customerId]
     );
-
     const checkGameId = await connection.query(
       `SELECT * FROM games WHERE id = $1`,
       [gameId]
@@ -26,17 +22,51 @@ export async function postRental(req, res) {
       `SELECT "stockTotal" FROM games WHERE id = $1`,
       [gameId]
     );
+
     if (checkGameStock.rows[0].stockTotal <= 0) {
       return res.status(400).send("Não há estoque do jogo");
     }
-    //verificar se numero de dias é maior do que 0
-    //verificar se tem o jogo disponivel
 
-    console.log(rentDate);
     console.log("Conferencias da database");
-    console.log(checkGameStock.rows[0].stockTotal);
 
-    res.status(201).send("aluguel criado");
+    const rentDate = dayjs().format("YYYY-MM-DD");
+    const returnDate = null;
+    const delayFee = null;
+
+    const pricePerDay = await connection.query(
+      `SELECT "pricePerDay" FROM games WHERE id = $1`,
+      [gameId]
+    );
+
+    const originalPrice = pricePerDay.rows[0].pricePerDay * daysRented;
+
+    const rentalArray = [
+      customerId,
+      gameId,
+      rentDate,
+      daysRented,
+      returnDate,
+      originalPrice,
+      delayFee,
+    ];
+
+    await connection.query(
+      `INSERT INTO rentals ("customerId",
+      "gameId",
+      "rentDate",
+      "daysRented",
+      "returnDate",
+      "originalPrice",
+      "delayFee") VALUES ($1,$2,$3,$4,$5,$6,$7)`,
+      rentalArray
+    );
+
+    await connection.query(
+      `UPDATE games SET "stockTotal" = "stockTotal"-1 WHERE id=$1`,
+      [gameId]
+    );
+
+    res.sendStatus(201);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
