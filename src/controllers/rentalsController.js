a4209caae20148a4bdd070a9bea073e58c35ae73
Thais <tfrancag@gmail.com>
2022-03-05 21:35:47 -0300

    feature. Adicionadas verificações de disponibilidade de jogo e existência de cliente via bando de dados na função de post do Rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,8 +1,41 @@
 import connection from "../database.js";
 
-export function postRental(req, res) {
+export async function postRental(req, res) {
   try {
+    const { customerId, gameId, daysRented } = req.body;
     //tem que adicionar coisas no body antes de enviar pra db. Suponho que é aqui que entram os joins
+    //COMEÇAR A CRIAR O OBJETO FINAL
+
+    const rentDate = Date();
+
+    const checkCustomer = await connection.query(
+      `SELECT * FROM customers WHERE id = $1`,
+      [customerId]
+    );
+
+    const checkGameId = await connection.query(
+      `SELECT * FROM games WHERE id = $1`,
+      [gameId]
+    );
+
+    if (checkCustomer.rows.length === 0 || checkGameId.rows.length === 0) {
+      return res.sendStatus(400);
+    }
+
+    const checkGameStock = await connection.query(
+      `SELECT "stockTotal" FROM games WHERE id = $1`,
+      [gameId]
+    );
+    if (checkGameStock.rows[0].stockTotal <= 0) {
+      return res.status(400).send("Não há estoque do jogo");
+    }
+    //verificar se numero de dias é maior do que 0
+    //verificar se tem o jogo disponivel
+
+    console.log(rentDate);
+    console.log("Conferencias da database");
+    console.log(checkGameStock.rows[0].stockTotal);
+
     res.status(201).send("aluguel criado");
   } catch (error) {
     console.log(error);
--- src/middleware/rentalsValidation.js
+++ src/middleware/rentalsValidation.js
@@ -1,9 +1,13 @@
 export function validateRentalsSchema(schema) {
   return (req, res, next) => {
+    const { customerId, gameId, daysRented } = req.body;
     const validation = schema.validate(req.body);
     if (validation.error) {
       res.sendStatus(400);
       return;
+    }
+    if (daysRented <= 0 || customerId < 0 || gameId < 0) {
+      return res.sendStatus(400);
     }
     next();
   };
