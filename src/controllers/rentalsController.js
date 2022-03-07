fd9bf509d61dd30b2062168009354c3bc16016b4
Thais <tfrancag@gmail.com>
2022-03-07 11:00:53 -0300

    feat. Adicionadas funções de retorno para queryString de gameID e customerId para busca de rentals com filtro

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -53,6 +53,102 @@ export async function getRentals(req, res) {
   try {
     const { customerId, gameId } = req.query;
 
+    if (customerId) {
+      const rentalsCustomerFilter = await connection.query(
+        `SELECT rentals.*,
+       customers.id as "customerId", customers.name as "customerName",
+       games.id as "gameId", games.name as "gameName",
+       categories.id as "categorieId", categories.name as "categoriesName"
+      FROM rentals 
+      JOIN customers ON rentals."customerId"=customers.id AND "customerId"=$1
+      JOIN games ON rentals."gameId"=games.id AND "customerId"=$1
+      JOIN categories ON games."categoryId"=categories.id AND "customerId"=$1
+     `,
+        [customerId]
+      );
+      const rentalsListCustomer = rentalsCustomerFilter.rows.map((row) => {
+        const {
+          id,
+          customerId,
+          gameId,
+          rentDate,
+          daysRented,
+          originalPrice,
+          delayFee,
+          customerName,
+          gameName,
+          categorieId,
+          categoriesName,
+        } = row;
+        const rentalsDetails = {
+          id: id,
+          customerId: customerId,
+          gameId: gameId,
+          rentDate: rentDate,
+          daysRented: daysRented,
+          originalPrice: originalPrice,
+          delayFee: delayFee,
+          customer: { id: customerId, name: customerName },
+          game: {
+            id: gameId,
+            name: gameName,
+            categoryId: categorieId,
+            categoryName: categoriesName,
+          },
+        };
+        return rentalsDetails;
+      });
+      return res.status(200).send(rentalsListCustomer);
+    }
+
+    if (gameId) {
+      const rentalsGameFilter = await connection.query(
+        `SELECT rentals.*,
+       customers.id as "customerId", customers.name as "customerName",
+       games.id as "gameId", games.name as "gameName",
+       categories.id as "categorieId", categories.name as "categoriesName"
+      FROM rentals 
+      JOIN customers ON rentals."customerId"=customers.id AND "gameId"=$1
+      JOIN games ON rentals."gameId"=games.id AND "gameId"=$1
+      JOIN categories ON games."categoryId"=categories.id AND "gameId"=$1
+     `,
+        [gameId]
+      );
+      const rentalsListGame = rentalsGameFilter.rows.map((row) => {
+        const {
+          id,
+          customerId,
+          gameId,
+          rentDate,
+          daysRented,
+          originalPrice,
+          delayFee,
+          customerName,
+          gameName,
+          categorieId,
+          categoriesName,
+        } = row;
+        const rentalsDetails = {
+          id: id,
+          customerId: customerId,
+          gameId: gameId,
+          rentDate: rentDate,
+          daysRented: daysRented,
+          originalPrice: originalPrice,
+          delayFee: delayFee,
+          customer: { id: customerId, name: customerName },
+          game: {
+            id: gameId,
+            name: gameName,
+            categoryId: categorieId,
+            categoryName: categoriesName,
+          },
+        };
+        return rentalsDetails;
+      });
+
+      return res.status(200).send(rentalsListGame);
+    }
     const rentalsBase = await connection.query(
       `SELECT rentals.*,
        customers.id as "customerId", customers.name as "customerName",
@@ -64,7 +160,6 @@ export async function getRentals(req, res) {
       JOIN categories ON games."categoryId"=categories.id
      `
     );
-
     const rentalsList = rentalsBase.rows.map((row) => {
       const {
         id,
@@ -98,63 +193,7 @@ export async function getRentals(req, res) {
       return rentalsDetails;
     });
 
-    // console.log(rentalsList);
-    if (customerId) {
-      console.log("ai tem que procurar os rentals da pessoa");
-    }
-
-    console.log("chegou aqui");
-    if (gameId) {
-      const rentalsGameFilter = await connection.query(
-        `SELECT rentals.*,
-       customers.id as "customerId", customers.name as "customerName",
-       games.id as "gameId", games.name as "gameName",
-       categories.id as "categorieId", categories.name as "categoriesName"
-      FROM rentals 
-      JOIN customers ON rentals."customerId"=customers.id AND "gameId"=$1
-      JOIN games ON rentals."gameId"=games.id AND "gameId"=$1
-      JOIN categories ON games."categoryId"=categories.id AND "gameId"=$1
-      
-     `,
-        [gameId]
-      );
-      const rentalsListGame = rentalsBase.rows.map((row) => {
-        const {
-          id,
-          customerId,
-          gameId,
-          rentDate,
-          daysRented,
-          originalPrice,
-          delayFee,
-          customerName,
-          gameName,
-          categorieId,
-          categoriesName,
-        } = row;
-        const rentalsDetails = {
-          id: id,
-          customerId: customerId,
-          gameId: gameId,
-          rentDate: rentDate,
-          daysRented: daysRented,
-          originalPrice: originalPrice,
-          delayFee: delayFee,
-          customer: { id: customerId, name: customerName },
-          game: {
-            id: gameId,
-            name: gameName,
-            categoryId: categorieId,
-            categoryName: categoriesName,
-          },
-        };
-        return rentalsDetails;
-      });
-      console.log(rentalsGameFilter.rows);
-      console.log(rentalsListGame);
-    }
-
-    res.status(200).send("Chegou no rentals");
+    res.status(200).send(rentalsList);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
